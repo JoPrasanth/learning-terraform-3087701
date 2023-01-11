5341f180c66983dbd87242614be5d6e1cda2be3b
JoPrasanth <91119573+JoPrasanth@users.noreply.github.com>
2023-01-11 12:11:48 +0530

    alb 1

--- main.tf
+++ main.tf
@@ -14,11 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-# data "aws_vpc" "default" {
-#  default = true
-# }
-
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -39,7 +35,6 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  # vpc_security_group_ids = [aws_security_group.blog.id]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -47,14 +42,53 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-  # name = "blog_new"
   name = "blog"
 
-  # vpc_id = data.aws_vpc.default.id
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -62,41 +96,3 @@ module "blog_sg" {
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-# resource "aws_security_group" "blog" {
-  
-#   name = "blog"
-#   description = "Allow http and https in. Allow eveything out"
-
-#   vpc_id = data.aws_vpc.default.id
-# }
-
-# resource "aws_security_group_rule" "blog_http_in" {
-#   type = "ingress"
-#   from_port = 80
-#   to_port = 80
-#   protocol = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
-
-# resource "aws_security_group_rule" "blog_https_in" {
-#   type = "ingress"
-#   from_port = 443
-#   to_port = 443
-#   protocol = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
-
-# resource "aws_security_group_rule" "blog_everything_out" {
-#   type = "egress"
-#   from_port = 0
-#   to_port = 0
-#   protocol = "-1"
-#   cidr_blocks = ["0.0.0.0/0"]
-
-#   security_group_id = aws_security_group.blog.id
-# }
